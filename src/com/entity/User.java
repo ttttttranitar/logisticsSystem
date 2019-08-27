163a17074ef9c6e20407dd3783c7c4aa84a88ff8
zpf1997 <528310049@qq.com>
2019-08-27 16:24:37 +0800

    aaa

--- src/com/entity/User.java
+++ src/com/entity/User.java
@@ -1,4 +1,37 @@
 package com.entity;
 
-public class User {
+public class User {/*用户类*/
+    private int user_id;//用户id
+    private String user_name;//用户名
+    private String password;//密码
+
+    public User(int user_id, String user_name, String password) {
+        this.user_id = user_id;
+        this.user_name = user_name;
+        this.password = password;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getUser_name() {
+        return user_name;
+    }
+
+    public void setUser_name(String user_name) {
+        this.user_name = user_name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
