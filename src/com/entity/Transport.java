28e666e90192b21dc2372aaa5cf331e1736c5e7a
ttttttranitar <313157362@qq.com>
2019-08-29 18:10:49 +0800

    new entity

---
+++ src/com/entity/Transport.java
@@ -0,0 +1,103 @@
+package com.entity;
+
+public class Transport {
+    private String transport_id;
+    private String start_date;
+    private String end_date;
+    private String pay_type;
+    private String transport_fee;
+    private String insurance_fee;
+    private String transport_start;
+    private String transport_distinatin;
+    private String transport_info;
+
+
+
+    public Transport(String transport_id, String start_date, String end_date, String pay_type, String transport_fee, String insurance_fee, String transport_start, String transport_distinatin, String transport_info) {
+        this.transport_id = transport_id;
+        this.start_date = start_date;
+        this.end_date = end_date;
+        this.pay_type = pay_type;
+        this.transport_fee = transport_fee;
+        this.insurance_fee = insurance_fee;
+        this.transport_start = transport_start;
+        this.transport_distinatin = transport_distinatin;
+        this.transport_info = transport_info;
+    }
+
+    public Transport(){}
+
+    public String getTransport_id() {
+        return transport_id;
+    }
+
+    public void setTransport_id(String transport_id) {
+        this.transport_id = transport_id;
+    }
+
+    public String getStart_date() {
+        return start_date;
+    }
+
+    public void setStart_date(String start_date) {
+        this.start_date = start_date;
+    }
+
+    public String getEnd_date() {
+        return end_date;
+    }
+
+    public void setEnd_date(String end_date) {
+        this.end_date = end_date;
+    }
+
+    public String getPay_type() {
+        return pay_type;
+    }
+
+    public void setPay_type(String pay_type) {
+        this.pay_type = pay_type;
+    }
+
+    public String getTransport_fee() {
+        return transport_fee;
+    }
+
+    public void setTransport_fee(String transport_fee) {
+        this.transport_fee = transport_fee;
+    }
+
+    public String getInsurance_fee() {
+        return insurance_fee;
+    }
+
+    public void setInsurance_fee(String insurance_fee) {
+        this.insurance_fee = insurance_fee;
+    }
+
+    public String getTransport_start() {
+        return transport_start;
+    }
+
+    public void setTransport_start(String transport_start) {
+        this.transport_start = transport_start;
+    }
+
+    public String getTransport_distinatin() {
+        return transport_distinatin;
+    }
+
+    public void setTransport_distinatin(String transport_distinatin) {
+        this.transport_distinatin = transport_distinatin;
+    }
+
+    public String getTransport_info() {
+        return transport_info;
+    }
+
+    public void setTransport_info(String transport_info) {
+        this.transport_info = transport_info;
+    }
+
+
+}
