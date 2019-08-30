468222441f119c1e88883881f7a279400231e159
zpf1997 <528310049@qq.com>
2019-08-30 17:26:37 +0800

    aaa

--- src/com/dao/impl/CostDaoImpl.java
+++ src/com/dao/impl/CostDaoImpl.java
@@ -9,40 +9,41 @@ import java.sql.SQLException;
 public class CostDaoImpl extends DButil implements CostDao {
 
     @Override
-    public int save(Cost cost) throws SQLException {
-        String sql="insert into `cost`(`income`,`expenditure`,`do_business_income`,`insurance`,`not_do_business_income`,`do_business_cost`,`car_freight`,`truckage`,`administration_cost`,`work_cost`,`residential_cost`,`water_and_electricity_cost`,`telephone_cost`,`other`,`financial_cost`,`profit`,`salary`,`month`) values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";//18个？
-        return updateDB(sql,cost.getIncome(),cost.getExpenditure(),cost.getDo_business_income(),cost.getInsurance(),cost.getNot_do_business_income(),cost.getDo_business_cost(),cost.getCar_freight(),cost.getTruckage(),cost.getAdministration_cost(),cost.getWork_cost(),cost.getResidential_cost(),cost.getWater_and_electricity_cost(),cost.getTelephone_cost(),cost.getOther(),cost.getFinancial_cost(),cost.getProfit(),cost.getSalary(),cost.getMonth());
+    public double save(Cost cost) throws SQLException {
+        String sql="insert into `cost`(`income`,`payout`,`bizIncome`,`carriageMoney`,`insurance`,`unbizIncome`,`bizFee`,`carCarriage`,`conveyWage`,`manageFee`,`officeFee`,`houseRent`,`waterElecFee`,`phoneFee`,`other`,`financeFee`,`profit`,`wage`,`month`) values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";//18个？
+        return updateDB(sql,cost.getIncome(),cost.getPayout(),cost.getBizFee(),cost.getCarriageMoney(),cost.getInsurance(),cost.getUnbizIncome(),cost.getCarCarriage(),cost.getConveyWage(),cost.getManageFee(),cost.getOfficeFee(),cost.getHouseRent(),cost.getWaterElecFee(),cost.getPhoneFee(),cost.getOther(),cost.getFinanceFee(),cost.getProfit(),cost.getWage(),cost.getMonth());
     }
 
     @Override
     public Cost getcost() throws SQLException {
-        String sql = "SELECT `income`,`expenditure`,`do_business_income`,`insurance`,`not_do_business_income`,`do_business_cost`," +
-                "`car_freight`,`truckage`,`administration_cost`,`work_cost`,`residential_cost`,`water_and_electricity_cost`,`telephone_cost`," +
-                "`other`,`financial_cost`,`profit`,`salary`,`month` from `cost` ";
+        String sql = "SELECT `income`,`payout`,`bizIncome`,`carriageMoney`,`insurance`,`unbizIncome`," +
+                "`bizFee`,`carCarriage`,`conveyWage`,`manageFee`,`officeFee`,`houseRent`,`waterElecFee`," +
+                "`phoneFee`,`other`,`financeFee`,`profit`,`wage`,`month` from `cost` ";
         Cost cost;
         try {
             res =queryDB(sql);
             cost =null;
             if (res.next()){
                 cost = new Cost();
-                cost.setIncome(res.getInt("income"));
-                cost.setExpenditure(res.getInt("expenditure"));
-                cost.setDo_business_income(res.getInt("do_business_income"));
-                cost.setInsurance(res.getInt("insurance"));
-                cost.setNot_do_business_income(res.getInt("not_do_business_income"));
-                cost.setDo_business_cost(res.getInt("do_business_cost"));
-                cost.setCar_freight(res.getInt("car_freight"));
-                cost.setTruckage(res.getInt("truckage"));
-                cost.setAdministration_cost(res.getInt("administration_cost"));
-                cost.setWork_cost(res.getInt("work_cost"));
-                cost.setResidential_cost(res.getInt("residential_cost"));
-                cost.setWater_and_electricity_cost(res.getInt("water_and_electricity_cost"));
-                cost.setTelephone_cost(res.getInt("telephone_cost"));
-                cost.setOther(res.getInt("other"));
-                cost.setFinancial_cost(res.getInt("financial_cost"));
-                cost.setProfit(res.getInt("profit"));
-                cost.setSalary(res.getInt("salary"));
-                cost.setMonth(res.getInt("month"));
+                cost.setIncome(res.getDouble("income"));
+                cost.setPayout(res.getDouble("payout"));
+                cost.setBizIncome(res.getDouble("bizIncome"));
+                cost.setCarriageMoney(res.getDouble("carriageMoney"));
+                cost.setInsurance(res.getDouble("insurance"));
+                cost.setUnbizIncome(res.getDouble("unbizIncome"));
+                cost.setBizFee(res.getDouble("bizFee"));
+                cost.setCarCarriage(res.getDouble("carCarriage"));
+                cost.setConveyWage(res.getDouble("conveyWage"));
+                cost.setManageFee(res.getDouble("manageFee"));
+                cost.setOfficeFee(res.getDouble("officeFee"));
+                cost.setHouseRent(res.getDouble("houseRent"));
+                cost.setWaterElecFee(res.getDouble("waterElecFee"));
+                cost.setPhoneFee(res.getDouble("phoneFee"));
+                cost.setOther(res.getDouble("other"));
+                cost.setFinanceFee(res.getDouble("financeFee"));
+                cost.setProfit(res.getDouble("profit"));
+                cost.setWage(res.getDouble("wage"));
+                cost.setMonth(res.getString("month"));
             }
         }finally {
             closeALL(conn,stat,res);
