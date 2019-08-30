94e89bd0ec2918c8fd7c2bc70c0e59777cce3a1b
zpf1997 <528310049@qq.com>
2019-08-30 17:26:57 +0800

    aaa

--- src/com/entity/Cost.java
+++ src/com/entity/Cost.java
@@ -1,192 +1,203 @@
 package com.entity;
 
-public class Cost {
-    private int income;//收入
-    private int expenditure;//支出
-    private int do_business_income;//营业收入
-    private int insurance;//保险
-    private int not_do_business_income;//非营业收入
-    private int do_business_cost;//营业费用
-    private int car_freight;//车运费
-    private int truckage;//搬运费
-    private int administration_cost;//管理费用
-    private int work_cost;//办公费用
-    private int residential_cost;//房租费
-    private int water_and_electricity_cost;//水电费
-    private int telephone_cost;//电话费
-    private int other;//其他
-    private int financial_cost;//财务费用
-    private int profit;//利润
-    private int salary;//工资
-    private int month;//月份
+public class Cost {//资金类
+    private double income;//收入
+    private double payout;//支出
+    private double bizIncome;//营业收入
+    private double carriageMoney;//运费金额
+    private double insurance;//保险
+    private double unbizIncome;//非营业收入
+    private double bizFee;//营业费用
+    private double carCarriage;//车运费
+    private double conveyWage;//搬运工资
+    private double manageFee;//管理费用
+    private double officeFee;//办公费用
+    private double houseRent;//房租费
+    private double waterElecFee;//水电费
+    private double phoneFee;//电话费
+    private double other;//其他
+    private double financeFee;//财务费用
+    private double profit;//利润
+    private double wage;//工资
+    private String month;//月份
 
     public Cost() {
     }
 
-    public Cost(int income, int expenditure, int do_business_income, int insurance, int not_do_business_income, int do_business_cost, int car_freight, int truckage, int administration_cost, int work_cost, int residential_cost, int water_and_electricity_cost, int telephone_cost, int other, int financial_cost, int profit, int salary, int month) {
+    public Cost(double income, double payout, double bizIncome, double carriageMoney, double insurance, double unbizIncome, double bizFee, double carCarriage, double conveyWage, double manageFee, double officeFee, double houseRent, double waterElecFee, double phoneFee, double other, double financeFee, double profit, double wage, String month) {
         this.income = income;
-        this.expenditure = expenditure;
-        this.do_business_income = do_business_income;
+        this.payout = payout;
+        this.bizIncome = bizIncome;
+        this.carriageMoney = carriageMoney;
         this.insurance = insurance;
-        this.not_do_business_income = not_do_business_income;
-        this.do_business_cost = do_business_cost;
-        this.car_freight = car_freight;
-        this.truckage = truckage;
-        this.administration_cost = administration_cost;
-        this.work_cost = work_cost;
-        this.residential_cost = residential_cost;
-        this.water_and_electricity_cost = water_and_electricity_cost;
-        this.telephone_cost = telephone_cost;
+        this.unbizIncome = unbizIncome;
+        this.bizFee = bizFee;
+        this.carCarriage = carCarriage;
+        this.conveyWage = conveyWage;
+        this.manageFee = manageFee;
+        this.officeFee = officeFee;
+        this.houseRent = houseRent;
+        this.waterElecFee = waterElecFee;
+        this.phoneFee = phoneFee;
         this.other = other;
-        this.financial_cost = financial_cost;
+        this.financeFee = financeFee;
         this.profit = profit;
-        this.salary = salary;
+        this.wage = wage;
         this.month = month;
     }
 
-    public void setIncome(int income) {
-        this.income = income;
+    public Cost(double income, double payout, double bizIncome, double carriageMoney, double insurance, double unbizIncome, double bizFee, double carCarriage, double conveyWage, double manageFee, double officeFee, double houseRent, double waterElecFee, double phoneFee, double other, double financeFee, double profit, double wage, double month) {
     }
 
-    public void setExpenditure(int expenditure) {
-        this.expenditure = expenditure;
-    }
-
-    public void setDo_business_income(int do_business_income) {
-        this.do_business_income = do_business_income;
-    }
-
-
-    public void setInsurance(int insurance) {
-        this.insurance = insurance;
-    }
-
-    public void setNot_do_business_income(int not_do_business_income) {
-        this.not_do_business_income = not_do_business_income;
-    }
-
-    public void setDo_business_cost(int do_business_cost) {
-        this.do_business_cost = do_business_cost;
-    }
-
-    public void setCar_freight(int car_freight) {
-        this.car_freight = car_freight;
-    }
-
-    public void setTruckage(int truckage) {
-        this.truckage = truckage;
-    }
-
-    public void setAdministration_cost(int administration_cost) {
-        this.administration_cost = administration_cost;
-    }
-
-    public void setWork_cost(int work_cost) {
-        this.work_cost = work_cost;
-    }
-
-    public void setResidential_cost(int residential_cost) {
-        this.residential_cost = residential_cost;
-    }
-
-    public void setWater_and_electricity_cost(int water_and_electricity_cost) {
-        this.water_and_electricity_cost = water_and_electricity_cost;
-    }
-
-    public void setTelephone_cost(int telephone_cost) {
-        this.telephone_cost = telephone_cost;
-    }
-
-    public void setOther(int other) {
-        this.other = other;
-    }
-
-    public void setFinancial_cost(int financial_cost) {
-        this.financial_cost = financial_cost;
-    }
-
-    public void setProfit(int profit) {
-        this.profit = profit;
-    }
-
-    public void setSalary(int salary) {
-        this.salary = salary;
-    }
-
-    public void setMonth(int month) {
-        this.month = month;
-    }
-
-    public int getIncome() {
+    public double getIncome() {
         return income;
     }
 
-    public int getExpenditure() {
-        return expenditure;
+    public double getPayout() {
+        return payout;
     }
 
-    public int getDo_business_income() {
-        return do_business_income;
+    public double getBizIncome() {
+        return bizIncome;
     }
 
+    public double getCarriageMoney() {
+        return carriageMoney;
+    }
 
-    public int getInsurance() {
+    public double getInsurance() {
         return insurance;
     }
 
-    public int getNot_do_business_income() {
-        return not_do_business_income;
+    public double getUnbizIncome() {
+        return unbizIncome;
     }
 
-    public int getDo_business_cost() {
-        return do_business_cost;
+    public double getBizFee() {
+        return bizFee;
     }
 
-    public int getCar_freight() {
-        return car_freight;
+    public double getCarCarriage() {
+        return carCarriage;
     }
 
-    public int getTruckage() {
-        return truckage;
+    public double getConveyWage() {
+        return conveyWage;
     }
 
-    public int getAdministration_cost() {
-        return administration_cost;
+    public double getManageFee() {
+        return manageFee;
     }
 
-    public int getWork_cost() {
-        return work_cost;
+    public double getOfficeFee() {
+        return officeFee;
     }
 
-    public int getResidential_cost() {
-        return residential_cost;
+    public double getHouseRent() {
+        return houseRent;
     }
 
-    public int getWater_and_electricity_cost() {
-        return water_and_electricity_cost;
+    public double getWaterElecFee() {
+        return waterElecFee;
     }
 
-    public int getTelephone_cost() {
-        return telephone_cost;
+    public double getPhoneFee() {
+        return phoneFee;
     }
 
-    public int getOther() {
+    public double getOther() {
         return other;
     }
 
-    public int getFinancial_cost() {
-        return financial_cost;
+    public double getFinanceFee() {
+        return financeFee;
     }
 
-    public int getProfit() {
+    public double getProfit() {
         return profit;
     }
 
-    public int getSalary() {
-        return salary;
+    public double getWage() {
+        return wage;
     }
 
-    public int getMonth() {
+    public String getMonth() {
         return month;
+    }
+
+    public void setIncome(double income) {
+        this.income = income;
+    }
+
+    public void setPayout(double payout) {
+        this.payout = payout;
+    }
+
+    public void setBizIncome(double bizIncome) {
+        this.bizIncome = bizIncome;
+    }
+
+    public void setCarriageMoney(double carriageMoney) {
+        this.carriageMoney = carriageMoney;
+    }
+
+    public void setInsurance(double insurance) {
+        this.insurance = insurance;
+    }
+
+    public void setUnbizIncome(double unbizIncome) {
+        this.unbizIncome = unbizIncome;
+    }
+
+    public void setBizFee(double bizFee) {
+        this.bizFee = bizFee;
+    }
+
+    public void setCarCarriage(double carCarriage) {
+        this.carCarriage = carCarriage;
+    }
+
+    public void setConveyWage(double conveyWage) {
+        this.conveyWage = conveyWage;
+    }
+
+    public void setManageFee(double manageFee) {
+        this.manageFee = manageFee;
+    }
+
+    public void setOfficeFee(double officeFee) {
+        this.officeFee = officeFee;
+    }
+
+    public void setHouseRent(double houseRent) {
+        this.houseRent = houseRent;
+    }
+
+    public void setWaterElecFee(double waterElecFee) {
+        this.waterElecFee = waterElecFee;
+    }
+
+    public void setPhoneFee(double phoneFee) {
+        this.phoneFee = phoneFee;
+    }
+
+    public void setOther(double other) {
+        this.other = other;
+    }
+
+    public void setFinanceFee(double financeFee) {
+        this.financeFee = financeFee;
+    }
+
+    public void setProfit(double profit) {
+        this.profit = profit;
+    }
+
+    public void setWage(double wage) {
+        this.wage = wage;
+    }
+
+    public void setMonth(String month) {
+        this.month = month;
     }
 }
