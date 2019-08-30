9090da31e322f46fd10f02397e1c9ae2b6ec764f
zpf1997 <528310049@qq.com>
2019-08-30 17:26:47 +0800

    aaa

--- src/com/dao/CostDao.java
+++ src/com/dao/CostDao.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public interface CostDao {
     //新增费用
-    int save(Cost cost) throws SQLException;
+    double save(Cost cost) throws SQLException;
     //查询费用
     Cost getcost()throws SQLException;
 }
